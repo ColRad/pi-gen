c1046c0478f8dc48de7eff403c1e478fea86bb14
colrad <collin.raddatz@gmail.com>
2023-09-20 10:02:57 +0200

    Remove unnecessary packages

--- stage2/02-net-tweaks/01-run.sh
+++ stage2/02-net-tweaks/01-run.sh
@@ -2,9 +2,11 @@
 
 install -v -d					"${ROOTFS_DIR}/etc/wpa_supplicant"
 install -v -m 600 files/wpa_supplicant.conf	"${ROOTFS_DIR}/etc/wpa_supplicant/"
+#install -v -d 					"${ROOTFS_DIR}/etc/systemd/system/dhcpcd@.service.d/"
+#install -v -m 755 files/no-wait.conf	"${ROOTFS_DIR}/etc/systemd/system/dhcpcd@.service.d/"
 
 on_chroot << EOF
-	SUDO_USER="${FIRST_USER_NAME}" raspi-config nonint do_boot_wait 0
+	SUDO_USER="${FIRST_USER_NAME}" raspi-config nonint do_boot_wait 1
 	SUDO_USER="${FIRST_USER_NAME}" raspi-config nonint do_netconf 1
 EOF
 
