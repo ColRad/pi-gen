6e13fde81e6b4a57e211ded51441b2cc841b2132
colrad <collin.raddatz@gmail.com>
2023-09-20 20:10:46 +0200

    Install all bootfiles

--- stage1/00-boot-files/00-run.sh
+++ stage1/00-boot-files/00-run.sh
@@ -2,11 +2,12 @@
 
 install -m 644 files/cmdline.txt "${ROOTFS_DIR}/boot/"
 install -m 644 files/config.txt "${ROOTFS_DIR}/boot/"
+install -m 644 files/config-nvme.txt "${ROOTFS_DIR}/boot/"
 install -m 644 files/cmdline-nvme.txt "${ROOTFS_DIR}/boot/"
 install -m 644 files/rootfsNvme.cpio.zst "${ROOTFS_DIR}/boot/"
 install -m 644 files/zImageNvme "${ROOTFS_DIR}/boot/"
+install -m 644 files/config-emmc.txt "${ROOTFS_DIR}/boot/"
 install -m 644 files/cmdline-emmc.txt "${ROOTFS_DIR}/boot/"
 install -m 644 files/rootfsEmmc.cpio.zst "${ROOTFS_DIR}/boot/"
 install -m 644 files/zImageEmmc "${ROOTFS_DIR}/boot/"
-
-
+install -m 644 files/initrd.img-6.1.21-v8+ "${ROOTFS_DIR}/boot/"
