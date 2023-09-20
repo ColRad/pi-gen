3f223661f688260f83ad38f9f73a52a3cf3b1bdb
colrad <collin.raddatz@gmail.com>
2023-09-20 19:23:25 +0200

    Trippleboot

--- stage1/00-boot-files/00-run.sh
+++ stage1/00-boot-files/00-run.sh
@@ -1,9 +1,12 @@
 #!/bin/bash -e
 
 install -m 644 files/cmdline.txt "${ROOTFS_DIR}/boot/"
-install -m 644 files/cmdline-gadget.txt "${ROOTFS_DIR}/boot/"
 install -m 644 files/config.txt "${ROOTFS_DIR}/boot/"
-install -m 644 files/rootfs.cpio.zst "${ROOTFS_DIR}/boot/"
-install -m 644 files/zImage "${ROOTFS_DIR}/boot/"
+install -m 644 files/cmdline-nvme.txt "${ROOTFS_DIR}/boot/"
+install -m 644 files/rootfsNvme.cpio.zst "${ROOTFS_DIR}/boot/"
+install -m 644 files/zImageNvme "${ROOTFS_DIR}/boot/"
+install -m 644 files/cmdline-emmc.txt "${ROOTFS_DIR}/boot/"
+install -m 644 files/rootfsEmmc.cpio.zst "${ROOTFS_DIR}/boot/"
+install -m 644 files/zImageEmmc "${ROOTFS_DIR}/boot/"
 
 
